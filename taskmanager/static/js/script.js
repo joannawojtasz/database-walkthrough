9177d70bcf22de70d3da0d617c0272e4340710c8
joannawojtasz <joannawojtasz@gmail.com>
2021-09-11 13:35:21 +0000

    add edit tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,18 +4,17 @@ document.addEventListener("DOMContentLoaded", function() {
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    let datepicker = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
 
     // select initialization
-    let selects = document.querySelectorAll('select');
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 
-    // collapsibles initialization
-    let collapsibles = document.querySelectorAll('.collapsible');
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
     M.Collapsible.init(collapsibles);
-
 });
